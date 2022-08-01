5d3a0eda7c5bc9e6c7b2f4ed945f1327683036a2
Gustavo Sebold <gustavosebold027@gmail.com>
2022-07-31 21:36:09 -0300

    Feature: update user and delete user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 class UserController < ApplicationController
-  def new
+  def create
     location = Location.new(location_params)
     bd_location = Location.find_by(lat: location.lat, lng: location.lng)
 
@@ -9,20 +9,82 @@ class UserController < ApplicationController
       location = bd_location
     end
 
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if User.find_by(email: user.email)
-      @response = { status: 409, message: 'Email address already exists' }
-      render json: @response, status: :conflict
+    @user.location = location
+
+    if @user.save
+      @response = { message: 'User created successfully' }
+      render json: @response, status: :created
+    else
+      errors = @user.errors.map { |error| { "#{error.attribute}" => error.full_message } }
+
+      @response = { message: errors }
+      render json: @response, status: :bad_request
+    end
+  end
+
+  def update
+    @id = params[:id]
+
+    if @id.nil? || @id.empty? || @id.is_a?(Numeric)
+      @response = { message: 'User id must be provided' }
+      render json: @response, status: :unprocessable_entity
       return
     end
 
-    user.location = location
+    bd_user = User.find_by(id: @id)
 
-    if user.save
-      @response = { status: 201, message: 'User created successfully' }
-      render json: @response, status: :created
+    if bd_user.nil?
+      @response = { message: 'Unvalid user id' }
+      render json: @response, status: :unprocessable_entity
+      return
     end
+
+    @user = User.new(user_params)
+
+    location = Location.new(location_params)
+    bd_location = Location.find_by(lat: location.lat, lng: location.lng)
+
+    if bd_location.nil?
+      location.save
+    else
+      location = bd_location
+    end
+
+    @user.location = location
+
+    if @user.save
+      @response = { message: 'User updated successfully' }
+      render json: @response, status: :ok
+    else
+      errors = @user.errors.map { |error| { "#{error.attribute}" => error.full_message } }
+
+      @response = { message: errors }
+      render json: @response, status: :bad_request
+    end
+  end
+
+  def delete
+    @id = params[:id]
+
+    if @id.nil? || @id.empty? || @id.is_a?(Numeric)
+      @response = { message: 'User id must be provided' }
+      render json: @response, status: :unprocessable_entity
+      return
+    end
+
+    user = User.find_by(id: @id)
+
+    if user.nil?
+      @response = { message: 'Unvalid user id' }
+      render json: @response, status: :unprocessable_entity
+      return
+    end
+
+    user.delete
+    @response = { message: 'User deleted successfully' }
+    render json: @response, status: :ok
   end
 
   private
