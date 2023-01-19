8251f7f770197c8d0dd8cd8589cc7843ec50f58e
Marielly Frozza <mariellyfrozza@gmail.com>
2023-01-19 14:52:36 -0300

    fix: :bug: Fixed favorite routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :user, param: :_id
   resources :pet, param: :id
 
-  get '/user/_id/favorites', to: 'user#favorites'
-  post '/user/_id/favorites', to: 'user#add_favorite'
-  delete '/user/_id/favorites', to: 'user#remove_favorite'
+  get '/user/:_id/favorites', to: 'user#favorites'
+  post '/user/:_id/favorites', to: 'user#add_favorite'
+  delete '/user/:_id/favorites', to: 'user#remove_favorite'
 
   patch '/pets/:id', to: 'pet#update'
   post '/auth/login', to: 'authentication#login'
