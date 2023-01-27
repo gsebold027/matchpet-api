5b07aa2d3c9244120437292ea2426dce596e6876
Gustavo Sebold <gustavosebold027@gmail.com>
2023-01-27 11:15:53 -0300

    fix: :bug: Adjust validation

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,15 +1,15 @@
-class ValidatorInterest < ActiveModel::Validator
-    def validate(record)
-        return record.errors.add :user, 'Duplicated interest' unless Interest.find_by(user: record.user,
-                                                                                      pet: record.pet).nil?
-    end
-end
-
 class Interest < ApplicationRecord
     belongs_to :user
     belongs_to :pet
 
-    validates_with ValidatorInterest, on: create
+    validates :duplicated?, on: create
 
     scope :filter_by_petId, ->(pet_id) { where(pet_id:) }
+
+    private
+
+    def duplicated?
+        return record.errors.add :user, 'Duplicated interest' unless Interest.find_by(user: self.user,
+                                                                                      pet: self.pet).nil?
+    end
 end
